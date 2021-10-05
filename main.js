f5f1adccb94c4173fc28021c60c622f30bc77040
oganov2001 <georgeoganov@hotmail.com>
2021-10-05 21:59:47 +0300

    Completed the course.

--- main.js
+++ main.js
@@ -1,6 +1,26 @@
-const btn = document.querySelector('.btn');
+const myForm = document.querySelector('#my-form');
+const nameInput = document.querySelector('#name');
+const emailInput = document.querySelector('#email');
+const msg = document.querySelector('.msg');
+const userList = document.querySelector('#users');
 
-btn.addEventListener('click', (e) => {
+myForm.addEventListener('submit', onSubmit);
+
+function onSubmit(e) {
   e.preventDefault();
-  console.log(e.target.className);
-});
+
+  if(nameInput.value === '' || emailInput.value === '') {
+    msg.classList.add('error');
+    msg.innerHTML = 'Please enter all fields';
+
+    setTimeout(() => msg.remove(), 3000);
+  } else {
+    const li = document.createElement('li');
+    li.appendChild(document.createTextNode(`${nameInput.value} : ${emailInput.value}`));
+
+    userList.appendChild(li);
+
+    nameInput.value = '';
+    emailInput.value = '';
+  }
+}
